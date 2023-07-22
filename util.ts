e5198760ec59b823162aa39cb2f2158b152aa3f8
DaviSM <davimesquita@gmail.com>
2023-07-21 21:22:29 -0300

    jsonheaders

--- util.ts
+++ util.ts
@@ -1,61 +1,68 @@
 // https://community.cloudflare.com/t/timeout-with-fetch/25249/5
 
 export async function promiseTimeoutRace<T>(thePromise: Promise<T>, timeout = 120000): Promise<T | null> {
-    try {
-        let signal = new Promise(resolve => setTimeout(resolve, timeout));
+	try {
+		let signal = new Promise(resolve => setTimeout(resolve, timeout));
 
-        let race = await Promise.race([thePromise, signal])
+		let race = await Promise.race([thePromise, signal]);
 
-        if (race !== null && race !== undefined) {
-            // @ts-ignore
-            return race;
-        }
-    } catch (e) {
-        console.error("promiseTimeoutRace: ", e);
-    }
-    return null;
+		if (race !== null && race !== undefined) {
+			// @ts-ignore
+			return race;
+		}
+	} catch (e) {
+		console.error('promiseTimeoutRace: ', e);
+	}
+	return null;
 }
 
 export async function fetchWithTimeout(fetchPromise: Promise<Response>, timeout = 120000): Promise<Response | null> {
-    return promiseTimeoutRace(fetchPromise, timeout);
+	return promiseTimeoutRace(fetchPromise, timeout);
 }
 
 export async function readRequestBody(request: Request): Promise<any> {
-    try {
-        if (request.method !== "GET") {
-            let contentType = request.headers.get("content-type");
+	try {
+		if (request.method !== 'GET') {
+			let contentType = request.headers.get('content-type');
 
-            if (contentType.includes("application/json")) {
-                return await request.json();
-            } else if (contentType.includes("form")) {
-                let formData = await request.formData();
-                let body = {};
-                for (let entry of formData.entries()) {
-                    body[entry[0]] = entry[1];
-                }
-                return {text: body};
-            } else {
-                return {text: request.text()};
-            }
-        }
-    } catch (e) {
-        console.error("readRequestBody: ", e);
-    }
-    return {text: ""};
+			if (contentType.includes('application/json')) {
+				return await request.json();
+			} else if (contentType.includes('form')) {
+				let formData = await request.formData();
+				let body = {};
+				for (let entry of formData.entries()) {
+					body[entry[0]] = entry[1];
+				}
+				return { text: body };
+			} else {
+				return { text: request.text() };
+			}
+		}
+	} catch (e) {
+		console.error('readRequestBody: ', e);
+	}
+	return { text: '' };
 }
 
 export function postBearer(data: object, apikey: string): any {
-    return {
-        headers: {
-            Authorization: `Bearer ${apikey}`,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        method: "POST",
-        body: JSON.stringify(data),
-    }
+	return {
+		headers: {
+			Authorization: `Bearer ${apikey}`,
+			'Content-Type': 'application/json',
+			'Accept': 'application/json',
+		},
+		method: 'POST',
+		body: JSON.stringify(data),
+	};
 }
 
-export const NOT_FOUND = () => new Response("404 Not Found", {status: 404});
-export const UNPROCESSABLE_ENTITY = () => new Response("422 Unprocessable Content", {status: 422});
-export const INTERNAL_SERVER_ERROR = () => new Response("500 Internal Server Error", {status: 500});
+export const JSON_HEADER = {
+	'content-type': 'application/json;charset=UTF-8',
+};
+export const HTML_HEADER = {
+	'content-type': 'text/html;charset=UTF-8',
+};
+
+export const NOT_FOUND = () => new Response('404 Not Found', { status: 404 });
+export const UNPROCESSABLE_ENTITY = () => new Response('422 Unprocessable Content', { status: 422 });
+export const INTERNAL_SERVER_ERROR = () => new Response('500 Internal Server Error', { status: 500 });
