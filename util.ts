e0562d60cba321d9389f8cdc129f47c7fd3798c4
DaviSM <davimesquita@gmail.com>
2023-11-10 22:25:38 -0300

    timeout

--- util.ts
+++ util.ts
@@ -4,28 +4,48 @@ export function isEmpty(text: string): boolean {
 
 // https://community.cloudflare.com/t/timeout-with-fetch/25249/5
 
+export async function promiseTimeoutRace<T>(thePromise: Promise<T>, timeout = 120000): Promise<T | null> {
+    try {
+        let signal = new Promise(resolve => setTimeout(resolve, timeout));
+
+        let race = await Promise.race([thePromise, signal]);
+
+        if (race !== null && race !== undefined) {
+            // @ts-ignore
+            return race;
+        }
+    } catch (e) {
+        console.error('promiseTimeoutRace: ', e);
+    }
+    return null;
+}
+
+export async function fetchWithTimeout(fetchPromise: Promise<Response>, timeout = 120000): Promise<Response | null> {
+    return promiseTimeoutRace(fetchPromise, timeout);
+}
+
 export async function readRequestBody(request: Request) {
     try {
         if (request.method !== 'GET') {
             let contentType = request.headers.get('content-type');
 
-            if (includes(contentType , 'application/json')) {
+            if (includes(contentType, 'application/json')) {
                 return await request.json();
-            } else if (includes(contentType , 'form')) {
+            } else if (includes(contentType, 'form')) {
                 let formData = await request.formData();
                 let body = {};
                 formData.forEach((value, key) => {
                     body[key] = value;
                 });
-                return { text: body };
+                return {text: body};
             } else {
-                return { text: request.text() };
+                return {text: request.text()};
             }
         }
     } catch (e) {
         console.error('readRequestBody: ', e);
     }
-    return { text: '' };
+    return {text: ''};
 }
 
 export function postBearer(data: any, apikey: string): any {
@@ -47,11 +67,11 @@ export const HTML_HEADER = {
     'content-type': 'text/html;charset=UTF-8',
 };
 
-export const NOT_FOUND = () => new Response('404 Not Found', { status: 404 });
-export const UNPROCESSABLE_ENTITY = () => new Response('422 Unprocessable Content', { status: 422 });
-export const INTERNAL_SERVER_ERROR = () => new Response('500 Internal Server Error', { status: 500 });
+export const NOT_FOUND = () => new Response('404 Not Found', {status: 404});
+export const UNPROCESSABLE_ENTITY = () => new Response('422 Unprocessable Content', {status: 422});
+export const INTERNAL_SERVER_ERROR = () => new Response('500 Internal Server Error', {status: 500});
 
-export function includesArr(arr: [], item:string) {
+export function includesArr(arr: [], item: string) {
     for (let i = 0; i < arr.length; i++) {
         if (includes(arr[i], item)) {
             return true;
@@ -60,6 +80,6 @@ export function includesArr(arr: [], item:string) {
     return false;
 }
 
-export function includes(str: string, item:string) {
+export function includes(str: string, item: string) {
     return str === null ? false : str.indexOf(item) !== -1;
 }
